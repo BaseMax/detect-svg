14b883836c50ed5e770a3b0522b9ab2ff1d06a8c
Max Base <MaxBaseCode@Gmail.Com>
2020-07-13 18:24:34 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const regex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*\s*(?:\[?(?:\s*<![^>]*>\s*)*\]?)*[^>]*>\s*)?(?:<svg[^>]*>[^]*<\/svg>|<svg[^/>]*\/\s*>)\s*$/i
+
+const detectSVG = input => {
+    if(input==undefined||input==null)
+        return false
+    input=input.toString().replace(/\s*<!Entity\s+\S*\s*(?:"|')[^"]+(?:"|')\s*>/img, '')
+    input=input.replace(/<!--([\s\S]*?)-->/g, '')
+    return Boolean(input) && regex.test(input)
+}
+
+module.exports = detectSVG
