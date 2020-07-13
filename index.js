7861d681b92c1045b64ca4dc92ea9fc7cb12b28a
Max Base <MaxBaseCode@Gmail.Com>
2020-07-13 18:25:05 +0430

    Adding isBinary
    Reading `isBinary` from one similar project

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 
 const regex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*\s*(?:\[?(?:\s*<![^>]*>\s*)*\]?)*[^>]*>\s*)?(?:<svg[^>]*>[^]*<\/svg>|<svg[^/>]*\/\s*>)\s*$/i
 
+const isBinary = buffer => {
+    const isBuffer = Buffer.isBuffer(buffer)
+    for(let i = 0; i < 24; i++) {
+        const characterCode = isBuffer ? buffer[i] : buffer.charCodeAt(i)
+        if(characterCode === 65533 || characterCode <= 8) {
+            return true
+        }
+    }
+    return false
+}
+
 const detectSVG = input => {
     if(input==undefined||input==null)
         return false
     input=input.toString().replace(/\s*<!Entity\s+\S*\s*(?:"|')[^"]+(?:"|')\s*>/img, '')
     input=input.replace(/<!--([\s\S]*?)-->/g, '')
-    return Boolean(input) && regex.test(input)
+    return Boolean(input) && !isBinary(input) && regex.test(input)
 }
 
 module.exports = detectSVG
