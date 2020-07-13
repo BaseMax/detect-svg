a823e5163f5de935ae4b3eed3675ff97a94c02fc
Max Base <MaxBaseCode@Gmail.Com>
2020-07-13 19:10:02 +0430

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const detectSVG = require("./index.js");
 
 console.log("excepted", true, "and getting", detectSVG('<svg xmlns="http://www.w3.org/2000/svg"><path fill="#00CD9F"/></svg>') )
-console.log("excepted", true, "and getting", detectSVG('<svg xmlns="http://www.w3.org/2000/svg"><path fill="#00CD9F"/></svg>') )
 console.log("excepted", true, "and getting", detectSVG('<svg></svg>') )
 console.log("excepted", false, "and getting", detectSVG('<svg>') )
 console.log("excepted", false, "and getting", detectSVG('<html></html>') )
